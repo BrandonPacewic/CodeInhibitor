a291ee0382070fc7d636b2af2079d11904349dee
Brandon <pacewicbrandon@gmail.com>
2022-01-27 11:27:35 -0800

    inital commit

---
+++ main.py
@@ -0,0 +1,50 @@
+#! /usr/bin/python3
+
+import argparse
+import os
+import random
+import time
+
+def delete_line(fname: str, targetLine: int) -> None:
+	try:
+		with open(fname, 'r') as file:
+			lines = file.readlines()
+	except OSError:
+		print('File not found')
+		exit()
+
+	lines.pop(targetLine)
+
+	with open(fanme, 'w') as file:
+		file.writelines(lines)
+
+
+def get_line_count(fname: str) -> int:
+	try:
+		with open(fname, 'r') as file:
+			lines = file.readlines()
+	except OSError:
+		print('File not found')
+		exit()
+
+	return len(lines)
+
+
+def main():
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--file', '-f', type=str, help='file to delete lines from')
+	parser.add_argument('--interval', '-i', type=int, help='number of seconds per each deletion', default=60)
+	args = parser.parse_args()
+	
+	fname = args.file
+	interval = args.interval
+
+	while True:
+		time.sleep(interval)
+		lineCount = get_line_count(fname)
+
+		targetLine = random.randrange(0, lineCount)
+		delete_line(fname, targetLine)
+
+if __name__ == '__main__':
+	main()
